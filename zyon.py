18f31bc2aa69f223fe1652250194e9094df10afa
Michael Zampieri <michaelrzampieri2023@gmail.com>
2026-07-15 14:29:47 -0300

    Zyon aprende a dar respostas aleatorias

--- zyon.py
+++ zyon.py
@@ -1,8 +1,12 @@
+import random
 print("Zyon online.")
+respostas = ["Interessante.", "Sei...", "Conta mais.", "Uhum, sei."]
 while True:
     mensagem = input("Voce: ")
     if mensagem == "sair":
         print("Zyon: Ja vai? Tinha acabado de ficar interessante.")
         break
-    print(f"Zyon: {mensagem}")
+    resposta = random.choice(respostas)
+    print(f"Zyon: {resposta}")
+
 
