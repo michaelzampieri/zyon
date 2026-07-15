1cd092e9239613bc20a881b55decb9e71afbaefd
Michael Zampieri <michaelrzampieri2023@gmail.com>
2026-07-15 16:34:31 -0300

    Integração com a OpenAi realizada, Zyon respondendo as mensagens

--- zyon.py
+++ zyon.py
@@ -1,4 +1,10 @@
 import random
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+load_dotenv()
+chave = os.getenv("OPENAI_API_KEY")
+cliente = OpenAI(api_key=chave)
 print("Zyon online.")
 respostas = ["Interessante.", "Sei...", "Conta mais.", "Uhum, sei."]
 while True:
@@ -6,7 +12,13 @@ while True:
     if mensagem.lower() == "sair":
         print("Zyon: Ja vai? Tinha acabado de ficar interessante.")
         break
-    resposta = random.choice(respostas)
-    print(f"Zyon: {resposta}")
+    conclusao = cliente.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "user", "content": mensagem}
+        ]
+    )
+    texto = conclusao.choices[0].message.content
+    print(f"Zyon: {texto}")
 
 
