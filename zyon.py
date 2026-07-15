7a35b7ba43b413b15d5269d6e4c3879f25577110
Michael Zampieri <michaelrzampieri2023@gmail.com>
2026-07-15 14:37:06 -0300

    Correcao de caracteres digitados pelo usuario

--- zyon.py
+++ zyon.py
@@ -2,8 +2,8 @@ import random
 print("Zyon online.")
 respostas = ["Interessante.", "Sei...", "Conta mais.", "Uhum, sei."]
 while True:
-    mensagem = input("Voce: ")
-    if mensagem == "sair":
+    mensagem = input("Voce: ").strip()
+    if mensagem.lower() == "sair":
         print("Zyon: Ja vai? Tinha acabado de ficar interessante.")
         break
     resposta = random.choice(respostas)
